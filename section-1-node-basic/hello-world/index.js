65ffc73ca8abd71fc171f29517db44b9b1cc6ce1
buihien0109 <buihien01091997@gmail.com>
2021-03-04 23:51:05 +0700

    init

---
+++ section-1-node-basic/hello-world/index.js
@@ -0,0 +1,17 @@
+console.log('Hello world');
+
+// Variable
+const a = [1,2,3,4,5]
+console.log(a)
+
+// Loop
+a.forEach(number => {
+    console.log(number)
+})
+
+// Function
+function sum(a,b) {
+    return a + b
+}
+
+console.log(sum(3,4))
